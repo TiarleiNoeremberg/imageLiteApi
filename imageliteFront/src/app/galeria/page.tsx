93d7a86e9ac0730fd4d0a60ae095889cba2e81c4
TiarleiNoeremberg <tiarlei.com@gmail.com>
2024-01-04 19:59:21 -0300

    Componente ImageCard e Pagina Galeria

--- imageliteFront/src/app/galeria/page.tsx
+++ imageliteFront/src/app/galeria/page.tsx
@@ -1,9 +1,16 @@
-import {Template} from '../../components/template'
+import {Template, ImageCard} from '@/components'
 
 export default function GaleriaPage(){
     return (
         <Template>
-            <h1>Galeria</h1>
+            <section className='grid grid-cols-4 gap-8'>
+                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src='https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80'/>
+                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src='https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80'/>
+                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src='https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80'/>
+                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src='https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80'/>
+                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src='https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80'/>
+                
+            </section>
         </Template>
     )
 }
