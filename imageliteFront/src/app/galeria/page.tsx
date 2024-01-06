60e840b9f68b0fad98bf2f2ef93bf50a368c6dfe
TiarleiNoeremberg <tiarlei.com@gmail.com>
2024-01-05 21:48:04 -0300

    Trazendo as imagens atraves do FrontEnd

--- imageliteFront/src/app/galeria/page.tsx
+++ imageliteFront/src/app/galeria/page.tsx
@@ -1,35 +1,29 @@
 'use client'
 
-import {Template, ImageCard} from '@/components'
+import { Template, ImageCard } from '@/components'
 import { useState } from 'react'
+import { useImageService } from '@/resources/image/image.service'
+import { Image } from '@/resources/image/image.resource';
 
 export default function GaleriaPage(){
 
-    const image1 = 'https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638887178/EducationHub/photos/midtown-manhattan.jpg'
-    const image2 = 'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80'
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([])
 
-    const [codigoImagem, setCodigoImage] = useState<number>(2);
-    const [urlImage, setUrlImage] = useState<string>(image2);
-
-    function mudarImagem(){
-        if(codigoImagem == 1){
-            setCodigoImage(2)
-            setUrlImage(image2)
-        } else {
-            setCodigoImage(1)
-            setUrlImage(image1)
-        }
+    async function searchImages(){
+        const result = await useService.buscar();
+        setImages(result);
+        console.table(images)
 
     }
 
+    
     return (
         <Template>
-            <button className='bg-gray-500' onClick={mudarImagem}>Clique para mudar</button>
+            <button className='bg-gray-500' onClick={searchImages}>Clique para mudar</button>
             <section className='grid grid-cols-4 gap-8'>
-                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src={urlImage}/>
-                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src={urlImage}/>
-                <ImageCard nome='Paisagem' tamanho='10MB' dataUpload='01/01/2024' src={urlImage}/>
-                
+                <ImageCard nome="{nomeImage}" tamanho='10MB' dataUpload='01/01/2024' src=""/>
+                 
             </section>
         </Template>
     )
